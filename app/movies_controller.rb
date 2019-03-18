e2c19f7cc3eddaadd4e0c92e2bcf0c6b9c8e506f
ktochow1 <ktochow1@binghamton.edu>
2019-03-18 18:21:30 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -95,7 +95,7 @@ def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   Movie.destroy
-  __
+
 end
 
 def can_destroy_all_items_at_once
