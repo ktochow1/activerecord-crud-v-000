f47cda1a5ea9931b96d8f7f96adea10cdfaa89b0
ktochow1 <ktochow1@binghamton.edu>
2019-03-18 18:13:32 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -79,16 +79,8 @@ def can_be_found_updated_and_saved
 end
 
 def can_update_using_update_method
-  # Update movie title to "Wat, huh?"
   movie = Movie.create(title: "Wat?")
-  # movie.find_by(title: "Wat")
   movie.update(title: "Wat, huh?")
-  
-  
-# user = User.find_by(name: 'David')
-# user.update(name: 'Dave')
-  # __
-  # __
 end
 
 def can_update_multiple_items_at_once
