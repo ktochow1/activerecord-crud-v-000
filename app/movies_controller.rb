3726bf6e75e4cef78cbc28f06e059448aed54679
ktochow1 <ktochow1@binghamton.edu>
2019-03-18 18:20:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -88,7 +88,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  Movie.update_all 
+  Movie.update_all(title: "A Movie") 
  
 # User.update_all "max_login_attempts = 3, must_change_password = 'true'"
   # __
